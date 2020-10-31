c6e40f8725b80610f53ab8178fc7823ac9326a09
MALANKA96 <70839290+MALANKA96@users.noreply.github.com>
2020-10-31 11:48:35 +0300

    feat: add solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,31 +2,30 @@ const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
   arr: [],
- getLength() {
+getLength() {
   return this.arr.length;
 },
- addLink(value) {
+addLink(value) {
   this.arr.push(`( ${value} )`)
   return this;
-  },
-  removeLink(position) {
-    if ( position - 1 < 0 || !(typeof position === 'number') || this.getLength() < position  )
-     {
+},
+removeLink(position) {
+    if ( this.getLength() < position  || position - 1 < 0 ) {
       this.arr = [];
-      throw new Error();
+      throw Error;
     }
-    this.arr.splice(position-1 , 1);
+    this.arr.splice(position - 1 , 1);
     return this;
   },
-   reverseChain() {
- this.arr.reverse();
- return this;
+reverseChain() {
+    this.arr.reverse()
+    return this;
   },
- finishChain() {
+finishChain() {
     let result = this.arr.join('~~');
-    delete this.arr;
+    this.arr = [];
     return result;
-  },
+  }
 };
 
 module.exports = chainMaker;
